0c6a71e5790be9d47488e28e307f17fe75bf6f8b
Russell Tabata <Russell@eva.lan>
2022-01-21 18:24:09 -0500

    Initial TurtleBucks Commit

---
+++ example.js
@@ -0,0 +1,20 @@
+let TurtleBucks = new CryptoBlockchain();
+
+console.log("TurtleBucks mining in progress....");
+TurtleBucks.addNewBlock(
+  new CryptoBlock(1, "01/06/2020", {
+    sender: "Iris Ljesnjanin",
+    recipient: "Cosima Mielke",
+    quantity: 50
+  })
+);
+
+TurtleBucks.addNewBlock(
+  new CryptoBlock(2, "01/07/2020", {
+    sender: "Vitaly Friedman",
+    recipient: "Ricardo Gimenes",
+    quantity: 100
+  })
+);
+
+console.log(JSON.stringify(TurtleBucks, null, 4));
